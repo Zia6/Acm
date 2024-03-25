0bb4a875f5f91da737c2b77e352b888cb801e297
YuanShenKing.PC <20230404@gmail.com>
2024-03-25 21:46:58 +0800

    1606ac

--- UVA/UVa1606.cpp
+++ UVA/UVa1606.cpp
@@ -1,81 +1,60 @@
 #include <iostream>
-#include <cstdio>
 #include <algorithm>
 #include <vector>
+#include <cstdio>
+#include <cmath>
 using namespace std;
-const int maxn = 1005;
-int color[maxn];
-struct vec
-{
+const int maxn = 1e3+5;
+struct vec{
     int x;
     int y;
-    int operator^(const vec &v) const
-    {
+    double red;
+    int color;
+    int operator ^ (const vec& v) const {
         return x * v.y - y * v.x;
     }
-    vec operator+(const vec &v) const
-    {
-        return {x + v.x, y + v.y};
+    bool operator < (const vec &v) const{
+        return red < v.red;
     }
-    vec operator-(const vec &v) const
-    {
-        return {x - v.x, y - v.y};
-    }
-} v[maxn], ve;
-struct node
-{
-    int index;
-    int value;
-    bool operator<(const node &n) const
-    {
-        return value < n.value;
-    }
-} a[maxn];
-int tem = 0, cnt1 = 0, cnt2 = 0, ans = 0;
-void solve()
-{
-    int n;
-    while (scanf("%d", &n) != EOF && n)
-    {
-        ans = 0;
-        for (int i = 0; i < n; i++)
-        {
-            scanf("%d %d %d", &v[i].x, &v[i].y, &color[i]);
-            if (color[i])
-                cnt1++;
-            else
-                cnt2++;
+}v1[maxn],v2[maxn];
+int n = 0,ans = 0;
+void solve(int x){
+    int k = 0,cnt = 2;
+    for(int i = 0; i < n; i++){
+        if(x == i) continue;
+        v2[k].x = v1[i].x - v1[x].x;
+        v2[k].y = v1[i].y - v1[x].y;
+        if(v1[i].color){
+            v2[k].x = -v2[k].x;
+            v2[k].y = -v2[k].y;
         }
-        for (int i = 0; i < n; i++)
-        {
-            int cnt3 = 0, cnt4 = n - cnt1;
-            ve.x = v[i].x, ve.y = 0;
-            for (int j = 0; j < n; j++)
-            {
-                a[j].value = ((v[j] - v[i]) ^ ve);
-                a[j].index = j;
-            }
-            sort(a, a + n);
-            for (int j = 0; j < n; j++)
-            {
-                if(i == j)
-                    continue;
-                if (color[a[j].index])
-                {
-                    cnt3++;
-                    ans = max(ans, cnt3 + cnt4);
-                }
-                else
-                {
-                    ans = max(ans, cnt3 + cnt4);
-                    cnt4--;
-                }
-            }
+        v2[k].red = atan2(v2[k].y,v2[k].x);
+        k++;
+    }
+    sort(v2,v2 + k);
+    int l = 0 ,r = 0;
+    while(l < k){
+        if( l == r) r = (r + 1) % k , cnt++;
+        while(l != r && (v2[l] ^ v2[r]) >= 0){
+            r = (r + 1) % k;
+            cnt++;
         }
-        printf("%d\n", ans);
+        cnt--;
+        ans = max(ans,cnt);
+        l++;
     }
 }
 int main()
 {
-    solve();
+    while(scanf("%d",&n)!=EOF && n){
+        for(int i = 0 ;i < n ;i++){
+            scanf("%d %d %d",&v1[i].x,&v1[i].y,&v1[i].color);
+        }
+        for(int i = 0; i < n; i++){
+            solve(i);
+        }
+            printf("%d\n",ans);
+            ans = 0;
+    }
+
 }
